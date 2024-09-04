6fc0605bcec984965c849268b69e03bab9fdb689
Paulo Ponciano <96355974+paulofponciano@users.noreply.github.com>
2024-09-04 11:16:07 -0300

    auth provider

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -26,7 +26,6 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 import { githubAuthApiRef } from '@backstage/core-plugin-api';
-import { SignInPage } from '@backstage/core-components';
 
 import {
   AlertDisplay,
