9e53d348887337c7937b82d7eae613f9cba8ec7a
portelagabriel <gportela@valcann.com.br>
2024-09-21 14:50:57 -0600

    add actions plugin

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -53,6 +53,11 @@ import {
 
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
+import { 
+  EntityGithubActionsContent, 
+  isGithubActionsAvailable,
+  EntityRecentGithubActionsRunsCard 
+} from '@backstage/plugin-github-actions';
 
 import {
   EntityKubernetesContent,
@@ -71,14 +76,11 @@ const cicdContent = (
   // This is an example of how you can implement your company's logic in entity page.
   // You can for example enforce that all components of type 'service' should use GitHubActions
   <EntitySwitch>
-    {/*
-      Here you can add support for different CI/CD services, for example
-      using @backstage-community/plugin-github-actions as follows:
+    {
       <EntitySwitch.Case if={isGithubActionsAvailable}>
         <EntityGithubActionsContent />
       </EntitySwitch.Case>
-     */}
-
+    }
     <EntitySwitch.Case>
       <EmptyState
         title="No CI/CD available for this entity"
@@ -141,6 +143,9 @@ const overviewContent = (
     </Grid>
     <Grid item md={8} xs={12}>
       <EntityHasSubcomponentsCard variant="gridItem" />
+    </Grid>
+    <Grid item sm={6}>
+      <EntityRecentGithubActionsRunsCard limit={4} variant="gridItem" />
     </Grid>
   </Grid>
 );
